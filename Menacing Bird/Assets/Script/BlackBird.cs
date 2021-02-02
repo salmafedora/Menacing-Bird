52d507692d5ab73c7ed6f293c9c6f6a2a55e903c
Salma Fedora <salma.fedora@gmail.com>
2021-02-02 20:08:14 +0700

    *M E N A C I N G*

---
+++ Menacing Bird/Assets/Script/BlackBird.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackBird : Bird
+{
+    [SerializeField]
+    public float _boomForce = 5000;
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+        if (col.gameObject.tag == "Enemy" || col.gameObject.tag == "Obstacle")
+        {
+            col.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(500, 500));
+            Destroy(gameObject);
+        }
+    }
+}
